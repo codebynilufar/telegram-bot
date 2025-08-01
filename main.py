79c6e2c9ca52b53dc0c625284f2de2649cf830fb
djumanov <djumanovdev@gmail.com>
2025-08-01 09:41:16 +0500

    echo main func

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from config import TOKEN
 from handlers import start, help, echo
 
 
-updater = Updater(TOKEN)
-dispatcher = updater.dispatcher
+def main() -> None:
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
 
-# command handler
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('help', help))
+    # command handler
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help))
 
-# message handler
-dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    # message handler
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
-updater.start_polling()
-updater.idle()
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
